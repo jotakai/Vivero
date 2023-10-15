2d529f404d9e0c690e26b544fb4aa14b40e11e6e
Gabriel <gabicordoba1998@gmail.com>
2023-10-15 11:42:41 -0300

    modificacion

--- Planta.h
+++ Planta.h
@@ -5,22 +5,23 @@ class Planta
 {
 public:
     Planta();
-    Planta(std::string nombre, std::string especie, int edad);
+    Planta(std::string nombre, std::string especie);
     ~Planta();
 
     std::string getNombre();
     std::string getEspecie();
-    int getEdad();
 
     void setNombre(std::string nombre);
     void setEspecie(std::string especie);
-    void setPrecio(float);
-float getPrecio();
+    void setPrecio(float);       //
+    float getPrecio();           //
+    void setCodigoPlanta(float); //
+    float getCodigoPlanta();     //
     void Cargar();
     void Mostrar();
 
 private:
-int _codigoPlanta;
+    int _codigoPlanta;
     std::string _nombre;
     std::string _especie;
     float _precio;
